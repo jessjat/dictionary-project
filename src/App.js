2704a9ee87238360ab7c5bad10f0d286a1896ce1
jessjat <mutanujat@gmail.com>
2025-06-30 13:07:35 +0300

    Added dictionary Api integration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import dictionary from "./dictionary.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       <img src={dictionary}className="App-pic img-fluid"alt="pic" /> 
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Jacinta Musau</small>
+      </footer>
+
+      </div>
     </div>
+
   );
 }
 
