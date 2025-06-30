e96b858eb85dbec69f9907b56b41e43f90889785
jessjat <mutanujat@gmail.com>
2025-06-30 13:10:56 +0300

    Added api documentation link

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -13,7 +13,7 @@ console.log(response.data[0]);
     function search(event){
         event.preventDefault();
     
-
+//documentation:http://dictionaryapi.dev/
         let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
 
